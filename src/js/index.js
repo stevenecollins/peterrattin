e5e57351349ef4f1be6da47f28c696406e2786fa
Steven Collins <me@stevenecollins.com>
2023-04-14 16:31:57 -0400

    Intitial Install

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// Javascript goes here
+window.onload = (event) => {
+  console.log("Steven, the page is fully loaded");
+  window.alert("Hello! Steven the Javascript is linked. You should also see a message in the console.");
+};
