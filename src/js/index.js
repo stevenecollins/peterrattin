e56e13dcd40cfc997eaa48041c96b8e0d3ed0c40
Steven Collins <me@stevenecollins.com>
2023-04-14 19:04:51 -0400

    added fonts

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,1 @@
-// Javascript goes here
-window.onload = (event) => {
-  console.log("Steven, the page is fully loaded");
-  window.alert("Hello! Steven the Javascript is linked. You should also see a message in the console.");
-};
+
