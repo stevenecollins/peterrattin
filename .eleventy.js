57a96e4177f2dd26f8c5fae97b460902a09ba5ec
Steven Collins <me@stevenecollins.com>
2023-04-26 18:32:47 -0400

    added first portfolio subpage

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addWatchTarget("./src/js/*");
